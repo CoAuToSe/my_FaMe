7db0060f6740a253b50d6b4576eba79824637fd7
CATS <Aurelien.Couthures@gmail.com>
2025-09-22 14:30:50 +0200

    made the namespace propagate to bpmn

--- fame_engine/launch/my_CATS.py
+++ fame_engine/launch/my_CATS.py
@@ -15,7 +15,7 @@ def generate_launch_description():
     share_directory = get_package_share_directory('fame_engine')
 
     launch_args = [
-        DeclareLaunchArgument('namespace', default_value='husky_a300'),
+        DeclareLaunchArgument('namespace', default_value='/a300_00041/platform'),
         DeclareLaunchArgument('bpmn', default_value='husky_a300_L')
     ]
     # revise path to your nodejs start file
