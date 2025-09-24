87494f0785f641fdb341fc6ad465c444070a0789
CATS <Aurelien.Couthures@gmail.com>
2025-09-24 13:10:43 +0200

    Update my_CATS.py

--- fame_engine/launch/my_CATS.py
+++ fame_engine/launch/my_CATS.py
@@ -15,7 +15,7 @@ def generate_launch_description():
     share_directory = get_package_share_directory('fame_engine')
 
     launch_args = [
-        DeclareLaunchArgument('namespace', default_value='/a300_00041/platform'),
+        DeclareLaunchArgument('namespace', default_value='/a300_00041'),
         DeclareLaunchArgument('bpmn', default_value='husky_a300_rostimer')
     ]
     # revise path to your nodejs start file
