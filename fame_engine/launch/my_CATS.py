2eba547e449bab913d0e7257d70457adada3fbb9
CATS <129091751+enstorso@users.noreply.github.com>
2025-10-22 13:27:47 +0200

    Update my_CATS.py

--- fame_engine/launch/my_CATS.py
+++ fame_engine/launch/my_CATS.py
@@ -16,7 +16,7 @@ def generate_launch_description():
 
     launch_args = [
         DeclareLaunchArgument('namespace', default_value='/a300_00041/platform'),
-        DeclareLaunchArgument('bpmn', default_value='diagram(5)')
+        DeclareLaunchArgument('bpmn', default_value='ping_pong')
     ]
     # revise path to your nodejs start file
     start_js_file = os.path.join(
